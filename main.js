4d6fa8257507ec647681d2bbb3334c77a5f85229
OlfaMas1 <60675554+OlfaMas1@users.noreply.github.com>
2020-02-06 13:05:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 function myFunction() {
     var rows = document.getElementById("rows").value;
-    
-    for (let i = 0; i < rows; i++) {
-        var output = '';
-        for (let j =0; j < rows - i; j++) output += ' ';
-        for (let k = 0; k <= i; k++) output += '# ';
-        console.log(output);  
+    if (rows > 25) {
+        console.clear();
+        document.getElementById("message").innerHTML = "merci d'entrer un nombre inferieur à 25";
     } 
+    if  (rows < 0){
+        console.clear();
+        document.getElementById("message").innerHTML = "les pyramides en negative n'existe pas .. merci d'entrer un nombre positive";
+        }
+    
+    else {
+        console.clear();
+        document.getElementById("message").innerHTML = "";
+        for (let i = 0; i < rows; i++) {
+            var output = '';
+            for (let j = 0; j < rows - i; j++) output += ' ';
+            for (let k = 0; k <= i; k++) output += '# ';
+            console.log(output);
+        }
+    }
 }
 
